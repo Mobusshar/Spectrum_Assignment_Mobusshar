ec1c0116c3531982dac88fb90983ce5c7a248a67
MitonmoyCoder <41119987+MitonmoyCoder@users.noreply.github.com>
2020-11-07 03:22:00 +0600

    Fetching google sheets data
    Fetching the datas from google sheets to python variables.

--- Assignment.py
+++ Assignment.py
@@ -1,5 +1,16 @@
-import os
-from Google import Create_Service
-import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("sample__AttendanceLog__2016").sheet1
+
+data = sheet.get_all_records()
+
+row = sheet.row_values(4)
+
+print (row)
